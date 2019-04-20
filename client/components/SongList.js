dc88dabce5db648aac3fe7ff13c61784b2cfa26c
jfedgar <jfedgar@gmail.com>
2019-04-20 00:13:37 -0500

    Add songlist component

---
+++ client/components/SongList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+
+export default class SongList extends Component {
+
+  render() {
+    return (
+      <div>
+        SongList
+      </div>
+    );
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
--- server/server.js
+++ server/server.js
@@ -8,16 +8,16 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://jarbjarb:flabdoodle@cluster0-qvv49.mongodb.net/test?retryWrites=true';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
